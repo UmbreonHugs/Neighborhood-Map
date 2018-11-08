7769cf0e7ce1aa144f9894e180afdf28b378a061
Luigi <thegaminggodlou@gmail.com>
2018-11-07 18:56:12 -0800

    improved tags, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Neighborhood-Map
-Neighborhood Map, showing the best places to eat in Downtown Tracy, CA!
+This is my Neighborhood Map, showing the best places to eat and drink in Downtown Tracy, CA!
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+##Description
+This is a single page application that is designed to map some of the best places to eat in Downtown Tracy. To use the app, search for a restaurant or type of cuisine (thai, coffee, bars, .ect) and it will show the results and map them
+
+# Requirements
+In order to install this project on your machine, you **must have Node.js installed on your machine**. If you do not have Node.js installed, you may install it [here](https://nodejs.org/en/).
+
+## Installation and Usage
+1. **Clone the repository** onto your PC. You can do so by running this command: `git clone https://github.com/UmbreonHugs/Neighborhood-Map.git`
+2. **Once cloned, install the dependencies required for the project.** You can do so by navigating to the index of the project, and running this command: `npm install`. This process will take several minutes.
+3. **Launch the application once the dependencies are installed.** This can be done by running this command: `npm start`. A new browser window should open up, pointing to http://localhost:3000
+
+# Resources
+- Uses the Bootstrap 4 Framework for theme and responsiveness (https://getbootstrap.com/)
+- Uses Leaflet.JS to generate the map (https://leafletjs.com/)
+- Uses FourSquare to gather data and useful information about the restaurants (https://foursquare.com)
--- src/restaurantList.js
+++ src/restaurantList.js
@@ -2,7 +2,7 @@ export const locations = [{
             name: "Barista's",
             location: {lat: 37.738299, lng: -121.428354},
             address: "112 W. 10th Street, Tracy, CA",
-            tags: "coffee, cafe",
+            tags: "coffee, cafe, tea",
             foursquareId: "4b8016ccf964a520145130e3"
           }, {
             name: "Tracy Thai",
@@ -14,13 +14,13 @@ export const locations = [{
             name: "The Press",
             location: {lat: 37.738864, lng: -121.429076},
             address: "A Street, Tracy, CA",
-            tags: "bar, restaurant, wine, pub, beer",
+            tags: "bar, restaurant, wine, pub, beer, burgers, steak",
             foursquareId: "5a0e4af16f0aa20fbffe2c03"
           }, {
             name: "Delta Brews",
             location: {lat: 37.738598, lng: -121.427212},
             address: "47 W 10th St, Tracy, CA",
-            tags: "bar, restaurant, pub, beer, wine",
+            tags: "bar, restaurant, pub, beer, wine, american, burgers, steak",
             foursquareId: "549f4134498ebcdda7412125"
           }, {
             name: "Sweet Arts",
@@ -32,35 +32,42 @@ export const locations = [{
             name: "THE|COMMONS",
             location: {lat: 37.738676, lng: -121.427106},
             address: "49 W 10th St, Tracy, CA",
+            tags: "bar, restaurant, pub, beer, wine, american, steak",
             foursquareId: "534c5210498e28c868cd9ff3"
           }, {
             name: "Bistro 135",
             location: {lat: 37.73876, lng: -121.428705},
             address: "135 W 10th St, Tracy, CA",
+            tags: "bar, restaurant, pub, beer, wine, american, steakhouse",
             foursquareId: "5704519d498e87cf57eb55d5"
           }, {
             name: "Helm's Ale House",
             location: {lat: 37.734977, lng: -121.425592},
             address: "600 N Central Ave, Tracy, CA",
+            tags: "bar, restaurant, pub, beer, wine, ale, american, salad",
             foursquareId: "4bdb23b5fed22d7f607e57c9"
           }, {
             name: "Thai Cafe",
             location: {lat: 37.73512, lng: -121.425557},
             address: "614 N Central Ave, Tracy, CA",
+            tags: "restaurant, food, thai",
             foursquareId: "4bcf4d55caff952169e7ccf0"
           }, {
             name: "Magellan's Restaurant",
             location: {lat: 37.734987, lng: -121.425287},
             address: "15 E 6th St, Tracy, CA",
+            tags: "restaurant, food, italian, wine, salad",
             foursquareId: "4c055e1e9a7920a1308ad279"
           }, {
             name: "Thai Jasmine",
             location: {lat: 37.738674, lng: -121.426778},
             address: "29 W 10th St, Tracy, CA",
+            tags: "restaurant, food, thai",
             foursquareId: "538691ef498edfec60a568f5"
           }, {
             name: "World Coffee House",
             location: {lat: 37.738723, lng: -121.427695},
             address: "69 W 10th St, Tracy, CA",
+            tags: "coffee, cafe, tea",
             foursquareId: "4dcedbc722718eed7a438b48"
           }]
